b3bd71cda8e7ca3e1b2ad6724c760c5cfe130172
Michael Rose <michael@rosenetwork.net>
2016-11-14 23:35:16 -0800

    current

--- src/clojure_test/core.clj
+++ src/clojure_test/core.clj
@@ -1,12 +1,27 @@
 (ns clojure-test.core
-  (:gen-class))
-(require '[clojure.string :as s])
+  (:gen-class)
+  (:refer-clojure
+   )
+  (:require [clojure.string :as string]
+            [com.rpl.specter :as specter :refer [transform]]
+            [puget.printer :as puget]
+            [thoughts.core :as wtf :refer [answer]]
+            [swiss.arrows :refer :all]
+            )
+  
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Fuck you very very much, World!")
   (println "Hello, World!"))
+
+(defn p [arg]
+  (puget/cprint arg))
+
+
+(defn any? [pred col] (not (not-any? pred col)))
 
 (defn error-message
   [severity]
@@ -26,6 +41,8 @@
              "DOOOOOOMED!")))
 
 (alt-error-message :mild)
+
+(alt-error-message :mild)
 (alt-error-message :other)
 (alt-error-message :dire)
 (error-message :mild)
@@ -39,7 +56,7 @@
 (defn arity
   "an example of different arities"
   ([]
-  (str "no args!"))
+   (str "no args!"))
   ([arg]
    (str "one arg and it is " arg))
   ([arg1 arg2]
@@ -56,7 +73,8 @@
   [chop-type & names]
   (map #(x-chop %1 chop-type) names))
 (x-chop "bob")
-(beat-group "death" "bob" "jan")
+(beat-group "death" "bob" "jan" "fred")
+(apply (partial beat-group "slap") [ "bob" "jane"])
 (defn announce-treasure-location
   [{lat :lat lng :lng}]
   (println (str "Treasure lat: " lat))
@@ -68,9 +86,9 @@
   (println (str "Treasure lng: " lng))
   treasure-location)
 
-;;================================================================
-;:                hobbit related stupidity                       =
-;;================================================================
+;================================================================
+;                hobbit related stupidity                       =
+;================================================================
 (def asym-hobbit-body-parts [{:name "head"           :size 3}
                              {:name "left-eye"       :size 1}
                              {:name "left-ear"       :size 1}
@@ -120,6 +138,12 @@
   [parts]
   (into [] (flatten (map both-parts parts))))
 
+
+(defn simpler-symetry2
+  "Expects a seq of maps what have a :name and :size"
+  [parts]
+  (flatten (into [] (map both-parts parts))))
+
 (defn symetrize-body-parts
   "Expects a seq of maps what have a :name and :size"
   [asym-body-parts]
@@ -130,9 +154,10 @@
         (recur remaining
                (into final-body-parts
                      (set [part (matching-part part)])))))))
-                        
 
- (def hobbit-body (symetrize-body-parts asym-hobbit-body-parts))                            
+
+(def hobbit-body (symetrize-body-parts asym-hobbit-body-parts))                            
+(reduce-to-symetry hobbit-body)
 
 (defn hit-hobbit
   [parts]
@@ -162,13 +187,19 @@
    {:month 3 :day 1 :human 4.2 :critter 3.3}
    {:month 3 :day 2 :human 4.0 :critter 3.8}
    {:month 4 :day 1 :human 3.7 :critter 3.9} 
-   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+   {:month 4 :day 2 :human 3.7 }])
+
 
 (defn max-consumed
   [key]
-  (if (every? #(contains? % key) food-journal)
-    (apply max (map key food-journal))
-    false))
+  (if (any? #(contains? % key) food-journal)
+    (apply max (filter #(not (nil? %)) (map #(key %) food-journal)))
+    0))
+
+(do (println  (max-consumed :human)
+              (max-consumed :critter)
+              (max-consumed :bullshit)))
+
 
 (defn within
   [low high number]
@@ -185,35 +216,150 @@
   (println bullshit)
   {:day day :month month})
 
-(defn p
-  [coll]
-  (map println coll))
-
 (defn bob [] (println "fuck you very much"))
 (defn sam [] (println "wtf"))
 (bob) 
 (sam) 
 (bob) 
-(println 3 (println 7)) 
+(println 3 (println 7))
+
+(defn head [col]
+  (take (- (count col) 1) col))
+
+(defn rev [col] (if (empty? col)
+               nil
+               (cons (last col) (rev (head col)))))
+
+;; (defn revr
+;;   ([remaining] (revr remaining '()))
+;;   ([remaining res] (if (empty? remaining)
+;;                res
+;;                (recur (head remaining)(concat res [(last remaining)])))))
+
+;; (defn revr2
+;;   ([col] (revr2 col '()))
+;;   ([col acc] (if (empty? col)
+;;                acc
+;;                (let [remaining (head col)
+;;                      result (concat acc [(last col)])]
+;;                  (recur remaining result)))))
+
+(defn rev
+  ([col] (rev col []))
+  ([col acc] (if (empty? col)
+               acc
+               (let [remaining (rest col)
+                     result (concat [(first col)] acc)]
+                 (recur remaining result)))))
+
+(rev '(1 2 3 4 5))
 
 
 
-;; (defn blarg [] #( * %1 %1 ))
+(defn intl
+  ([cols] (intl cols []))
+  ([cols acc] (if (some empty? cols)
+               acc
+               (let [remaining (map rest cols)
+                     result (concat acc (map first cols))]
+                 (recur remaining result)))))
+(intl [[1 2 3] [4 5]])
 
-; (defn even-numbers
-;   ([] (even-numbers 0))
-;   ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
 
-; (defn odd-numbers
-;   ([] (odd-numbers 1))
-;   ([n] (cons n (lazy-seq (odd-numbers (+ n 2))))))
+;;;-------------------------------------------------------
+(def filename "suspects.csv")
 
-; (defn sum
-;   ([vals]
-;    (sum vals 0))
-;   ([vals accumulating-total]
-;    (println vals)
-;    (println accumulating-total)
-;    (if (empty? vals)
-;      accumulating-total
-;      (recur (rest vals) (+ (first vals) accumulating-total)))))
+(defn str->int [str] (Integer. str))
+
+(defn split-csv [string]
+  (map #(string/split % #",")
+       (string/split string #"\n")))
+
+;; (map process-entry (split-csv (slurp filename)))
+
+(defn process-entry [entry]
+  {:name (first entry) :glitter-index (str->int (last entry))})
+
+(defn parse [file]
+  (->> file slurp split-csv (map process-entry)))
+
+(p (parse filename))
+
+(defn glitter-filter [min]
+  (filter #(>= (:glitter-index %) min) (parse filename)))
+
+(println (glitter-filter 0))
+(defn foobar
+  "not a real function"
+  [something & something-else]
+  (+ 2 6 (+ 7 2))
+  (str something "this" something-else))
+
+(defn barfoo
+  [something]
+  (+ 2 6)
+  (+ 7 9)
+  (+ 7 23))
+
+(foobar "fuck")
+(barfoo "fuck")
+(def blarg 9)
+(str (+ 2 blarg))
+
+(+ 2 2 (+ 2 7 9 8) 2 5 2)
+
+(reduce (fn [new-map [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30 :min 10})
+
+(defn my-updater [f my-map]
+  (reduce (fn [new-map [key val]]
+            (assoc new-map key (f val)))
+          {}
+          my-map))
+
+(defmacro infix
+  "infix macro"
+  [infixed]
+  (list (second infixed)
+        (first infixed)
+        (last infixed)))
+
+(infix (1 + 1 (infix (1 + 3))))
+(infix (1 + 3))
+(my-updater #(+ % 3) {:max 30 :min 10})
+(transform [:outer :max] (fn [n](+ 9 n)) {:outer {:max 30 :min 10}})
+(-<> 2
+     (* <> 5)
+     (vector 1 2 <> 3 4))
+(puget/cprint hobbit-body)
+(p hobbit-body)
+(+ 1 1)
+(defn xform [xs]
+  (-<> xs
+     (map #(+ 2 %) <>)
+     (filter odd? <>)
+     (apply + <>)))
+
+(xform (range 0 10))
+
+(defn atom? [x]
+  (and (not (seq? x))
+       (not (nil? x))))
+
+(defn lat? [l]
+  (every? atom? l))
+
+(defn member? [x col]
+  (cond
+    (empty? col) false
+    :else (or (identical? (first col) x)
+              (member? x (rest col)))))
+(defn rember [x col]
+  (cond
+    (empty? col) '()
+    (identical? (first col) x) (rest col)
+    :else (cons (first col)(rember x (rest col)))))
+
+(def ls [[:apple :peach :pumkin][:plum :pear :cherry]])
